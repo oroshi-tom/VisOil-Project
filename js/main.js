39b5bccc03305cebfbb9481176ff276a075a1270
oroshi_tom <thomas@oroshi.ca>
2021-03-09 07:36:14 -0700

    update About, Bio contents; Update scroll triggers

--- js/main.js
+++ js/main.js
@@ -1,73 +1,73 @@
 // Scroll to 'about'
-$('#view-about').on('click',function(){
-    const about = $('#about').position().top;
+$("#view-about").on("click", function () {
+  const about = $("#about").position().top;
 
-    $('html, body').animate(
-        {
-            scrollTop: about
-        },
-        900
-    );
+  $("html, body").animate(
+    {
+      scrollTop: about,
+    },
+    900
+  );
 });
 // Scroll to 'features'
-$('#view-features').on('click',function(){
-    const features = $('#features').position().top;
+$("#view-features").on("click", function () {
+  const features = $("#features").position().top;
 
-    $('html, body').animate(
-        {
-            scrollTop: features
-        },
-        900
-    );
+  $("html, body").animate(
+    {
+      scrollTop: features,
+    },
+    900
+  );
 });
 
 // Scroll to 'team'
-$('#view-team').on('click',function(){
-    const team = $('#team').position().top;
+$("#view-bio").on("click", function () {
+  const bio = $("#bio").position().top;
 
-    $('html, body').animate(
-        {
-            scrollTop: team
-        },
-        900
-    );
+  $("html, body").animate(
+    {
+      scrollTop: bio,
+    },
+    900
+  );
 });
 
 // Scroll to 'docs'
-$('#view-contact').on('click',function(){
-    const contact = $('#contact').position().top;
+$("#view-contact").on("click", function () {
+  const contact = $("#contact").position().top;
 
-    $('html, body').animate(
-        {
-            scrollTop: contact
-        },
-        900
-    );
+  $("html, body").animate(
+    {
+      scrollTop: contact,
+    },
+    900
+  );
 });
 
 // Scroll button
-var scrollBtn = document.getElementById('top-btn');
+var scrollBtn = document.getElementById("top-btn");
 
 // If user scrolls more than 20px, display button
-window.onscroll = function () {showButton()};
+window.onscroll = function () {
+  showButton();
+};
 
 function showButton() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        scrollBtn.style.display = 'block';
-    } else {
-        scrollBtn.style.display = 'none';
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
 }
 
 // Scroll to top if button clicked
-$('#top-btn').on('click', function(){
-    $('html, body').animate(
-        {
-            scrollTop: 0
-        }
-    )
+$("#top-btn").on("click", function () {
+  $("html, body").animate({
+    scrollTop: 0,
+  });
 });
 
 window.onload = () => {
-    $('body').fadeIn(500);
+  $("body").fadeIn(500);
 };
